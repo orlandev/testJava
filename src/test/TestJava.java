88ae099473df79b0f8b46c06f667af7c9d490529
Orlando N. Rodriguez <onovas05@gmail.com>
2022-06-15 13:42:34 -0400

    Initial commit

---
+++ src/test/TestJava.java
@@ -0,0 +1,15 @@
+package test;
+
+public class TestJava {
+
+
+
+    public static void main(String[] args) {
+
+        System.console().printf("int");
+        System.console().printf("char");
+        System.console().printf("boolean");
+        System.console().printf("String"); 
+    }
+
+}
